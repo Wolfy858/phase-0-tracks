f369589712500be70d27b3c12e772cbe263d3551
Elliot <Wolfy858@gmail.com>
2016-03-22 13:51:20 -0700

    added check for vampire allergies to sunshine, and note at end of program with plot twist

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -10,16 +10,28 @@ num_employees.times do
   likes_garlic = gets.chomp == "yes" ? true : false
   puts "Would you like to enroll in the company's health insurance?"
   healthcare_enrollment = gets.chomp == "yes" ? true : false
+  puts "Please list your allergies. Type 'done' when finished."
+  allergy = gets.chomp
+  until allergy == 'sunshine' || allergy == 'done'
+    allergy = gets.chomp
+  end
 
-  if (name == "Drake Cula" || name == "Tu Fang")
-    puts "Definitely a vampire"
-  elsif age > 100 && (!likes_garlic && !healthcare_enrollment)
-    puts "Almost certainly a vampire"
-  elsif age > 100 && (!likes_garlic || !healthcare_enrollment)
-    puts "Probably a vampire"
-  elsif age < 100 && (likes_garlic || healthcare_enrollment)
-    puts "Probably not a vampire."
+  if allergy == 'sunshine'
+    puts "Probably a vampire."
   else
-    puts "Results inconclusive."
+    if (name == "Drake Cula" || name == "Tu Fang")
+      puts "Definitely a vampire"
+    elsif age > 100 && (!likes_garlic && !healthcare_enrollment)
+      puts "Almost certainly a vampire"
+    elsif age > 100 && (!likes_garlic || !healthcare_enrollment)
+      puts "Probably a vampire"
+    elsif age < 100 && (likes_garlic || healthcare_enrollment)
+      puts "Probably not a vampire."
+    else
+      puts "Results inconclusive."
+    end
   end
 end
+
+puts "Actually, never mind! What do these questions have to do
+with anything? Let's all be friends."
