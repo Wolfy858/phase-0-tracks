6b9d2d6eed02f3aa988df1fc0e77c76055ec22be
Elliot <Wolfy858@gmail.com>
2016-04-04 15:32:12 -0700

    write methods for Santa class, and initialize method to set attributes. Write  basic driver code to test methods

---
+++ ruby/santa.rb
@@ -0,0 +1,40 @@
+class Santa
+
+
+  def speak
+    puts "Ho, ho, ho! Haaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie)
+    puts "That was a good #{cookie}"
+  end
+
+  def initialize(gender, ethnicity)
+    puts "Initializing Santa instance ..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer",
+       "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+  end
+
+end
+
+# s = Santa.new()
+# s.speak
+# s.eat_milk_and_cookies("wreath cookie")
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male",
+   "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African",
+   "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+def inspect
+  "#{@gender}, #{@ethnicity}"
+end
+
+p santas
