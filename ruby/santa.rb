4ca59208a513e3b2591c62769d53ff999361cf56
Elliot <Wolfy858@gmail.com>
2016-04-04 15:59:26 -0700

    Write celebrate_birthday, get_mad_at, and getter/setter methods for gender/ethnicity. Write corresponding driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,5 @@
 class Santa
+  attr_reader :reindeer_ranking, :age
 
 
   def speak
@@ -18,11 +19,35 @@ class Santa
     @age = 0
   end
 
-end
+  def celebrate_birthday
+    @age += 1
+  end
 
-# s = Santa.new()
-# s.speak
-# s.eat_milk_and_cookies("wreath cookie")
+  def get_mad_at(reindeer)
+    @reindeer_ranking.delete(reindeer)
+    @reindeer_ranking.push(reindeer)
+  end
+
+  def inspect
+    "gender: #{@gender},
+     ethnicity: #{@ethnicity},
+     age: #{@age}
+     "
+  end
+
+  def gender
+    @gender
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
+end
+#Driver code testing first three instance methods
+s = Santa.new("female", "orcish")
+s.speak
+s.eat_milk_and_cookies("wreath cookie")
 
 santas = []
 example_genders = ["agender", "female", "bigender", "male",
@@ -32,9 +57,14 @@ example_ethnicities = ["black", "Latino", "white", "Japanese-African",
 example_genders.length.times do |i|
   santas << Santa.new(example_genders[i], example_ethnicities[i])
 end
-
-def inspect
-  "#{@gender}, #{@ethnicity}"
-end
-
+#Driver code testing gender and ethnicity initialization
 p santas
+#Driver code testing #get_mad_at and #celebrate_birthday
+p s.reindeer_ranking
+s.get_mad_at("Dasher")
+p s.reindeer_ranking
+s.celebrate_birthday
+p s.age
+#Driver code testing getter methods
+p s.gender
+p s.ethnicity
