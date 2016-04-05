c0acd8f16b0b83bfcc8da4f478b7a829e8d1bde1
Elliot <Wolfy858@gmail.com>
2016-04-05 13:25:33 -0700

    Write a few instance methods for puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,32 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
+  
+  def speak(i)
+    i.times {puts ("woof")}
+  end
+
+  def roll_over
+    puts "roll over"
+  end
+
+  def dog_years(x)
+    return x*7
+  end
+
+  def hungry
+    return true
+  end
 
 end
 
+fido = Puppy.new
+fido.fetch("rubber duck")
+
+fido.speak(4)
+
+fido.roll_over
+
+puts fido.dog_years(4)
+
+puts fido.hungry
