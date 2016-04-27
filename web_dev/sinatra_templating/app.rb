df8653c14719ff0d372d7688c83a79603067c745
Elliot <Wolfy858@gmail.com>
2016-04-27 16:57:06 -0700

    Complete all work for exercise 9-5 Templating in Sinatra

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -17,11 +17,35 @@ get '/students/new' do
   erb :new_student
 end
 
+get '/students/delete' do
+  erb :delete_student
+end
+
 # create new students via
 # a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
+
+post '/students/delete' do
+  db.execute("DELETE FROM students WHERE name=(?)", [params[:name]])
+  redirect '/'
+end
+
+get '/students/:name/cohortmates' do
+  @campus_id = db.execute("SELECT campuses.id FROM students
+  JOIN campuses ON students.campus_id = campuses.id WHERE students.name = (?)", [params[:name]])
+  @cohortmates = db.execute("SELECT name FROM students WHERE campus_id = (?)", [@campus_id[0]['id']])
+  erb :cohortmates
+end
+
+
+
+
+
+# post '/students/:' do
+#   db.execute("UPDATE students SET name= (?) campus=(?) age=(?) WHERE  )
+# end
 
 # add static resources
